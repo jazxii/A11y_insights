73461e6963ad64dfdf1168223545586420ef4cff
Jassim Mohammed Shamim <72601716+jazxii@users.noreply.github.com>
2025-10-14 14:15:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
 
-from app.services.save_markdown import save_markdown_report
+from .save_markdown import save_markdown_report
 from .schemas import UserStoryIn, ReportOut, A11yDeepReportOut, DetectedComponent,A11yV3JSONOut
 from .parser import extract_components
 from .ai_client import analyze_with_ai, analyze_with_ai_v3, analyze_with_ai_v3json, analyze_with_ai_v4
